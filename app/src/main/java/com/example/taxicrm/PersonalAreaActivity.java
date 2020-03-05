b7bdfa20c2be2c2dc116ebcd9cd3501443dc0c85
KornevSergei <4Kornev@gmail.com>
2020-03-05 23:07:01 +0300

    Step 5

--- app/src/main/java/com/example/taxicrm/PersonalAreaActivity.java
+++ app/src/main/java/com/example/taxicrm/PersonalAreaActivity.java
@@ -2,7 +2,9 @@ package com.example.taxicrm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PersonalAreaActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class PersonalAreaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_area);
+    }
+
+    //описываем кнопку новости
+    public void news(View view) {
+        Intent intent = new Intent(PersonalAreaActivity.this, NewsActivity.class);
+        startActivity(intent);
     }
 }
